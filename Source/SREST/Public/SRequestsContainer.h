4feb1acf9350d46a82e4c160081d1a78034dc7b4
Sergei Lapkin <ser0inc@gmail.com>
2025-03-03 20:12:49 +0400

    UE-423  - Added requests container to REST for easy make handlers  - Added zip/unzip plugin for editor  - Begin implementation of editor REST API

---
+++ Source/SREST/Public/SRequestsContainer.h
@@ -0,0 +1,40 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "SRequestsContainer.generated.h"
+
+class USRequestManager;
+/**
+ * 
+ */
+UCLASS()
+class SREST_API USRequestsContainer : public UObject
+{
+	GENERATED_BODY()
+
+public:
+
+	USRequestsContainer();
+
+	UFUNCTION(BlueprintCallable)
+	void Setup(USRequestManager* InRequestManager);
+
+protected:
+
+	virtual void PostInitProperties() override;
+	
+	UFUNCTION(BlueprintNativeEvent)
+	void OnSetup(USRequestManager* InRequestManager);
+	
+	UPROPERTY(Transient)
+	USRequestManager* RequestManager;
+
+	UPROPERTY(EditDefaultsOnly, Category = Configuration)
+	bool bUseInternalManager = false;
+	
+	UPROPERTY(EditDefaultsOnly, Category = Configuration)
+	FString InternalManagerEndpoint;
+};
